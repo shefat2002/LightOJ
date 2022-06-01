e23da5f71cf53c24c17fb25ee69b420aa0c8907c
Shefat Al Mahmud <shefat2002@gmail.com>
2022-06-02 04:05:03 +0600

    Create Positive Negative Sign.cpp

---
+++ Positive Negative Sign.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define nl cout << "\n";
+#define pi (2*acos(0))
+#define case cout << "Case " << i+1 << ": ";
+
+void fast()
+{
+    ios_base::sync_with_stdio(false);cin.tie(NULL);
+}
+
+int main(int argc , char *argv[])
+{
+    int a , i = 0; cin >> a;
+    while(a--){
+        long long n , m;
+        cin >> n >> m;
+        case; i++;
+        long long  sum = 0 ;
+        sum = (n/2)*m;
+        cout << sum;
+        nl;
+    }
+    return 0;
+}
+
+
+
