5f552f585fea7244986996b9158181df2e14eb62
Shefat Al Mahmud <shefat2002@gmail.com>
2022-08-15 21:13:46 +0600

    Create 1043-Triangle Partitioning.cpp

---
+++ 1043-Triangle Partitioning.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll      long long
+#define nl      cout << "\n";
+#define pi      (2*acos(0))
+#define case    cout << "Case " << case_number << ": "; case_number++;
+#define fast    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define MAX_INT 1000000007
+#define YES     "Yes"
+#define NO      "No"
+
+int case_number = 1;
+//knights move
+int dr[] = {2, 2, -2, -2, 1, 1, -1, -1};
+int dc[] = {1, -1, 1, -1, 2, -2, 2, -2};
+
+
+void file();
+/*----------------------------------------------------------------------------------------------------------------------------------------*/
+
+
+void solve()
+{
+    double ab, ac, bc, r;
+    cin >> ab >> ac >> bc >> r;
+    cout << fixed << setprecision(15) << sqrt(r/(r+1))*ab;
+
+}
+
+
+int main()
+{
+    file();
+    int n; 
+    //solve();
+    //cin >> n; while(n--){solve();nl;}     // CF
+    cin >> n; while(n--){case;solve();nl;}  // Case 
+
+    return 0;
+}
+
+void file()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt" , "r", stdin);
+    freopen("out.txt" , "w", stdout);
+    #endif
+}
